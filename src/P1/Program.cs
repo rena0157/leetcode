b60306adaf996756cfc709154bb034bde7878cde
Adam Renaud <adam.renaud@abicbuilds.com>
2021-09-20 21:33:47 -0400

    adding problem 1

---
+++ src/P1/Program.cs
@@ -0,0 +1,50 @@
+﻿// leetcode - Two Sums
+
+using System;
+using P1;
+
+Example1();
+Example2();
+Example3();
+Example4();
+
+void Example1()
+{
+    var solution = new Solution();
+    var nums = new[] { 2, 7, 11, 15 };
+    const int target = 9;
+
+    var example = solution.TwoSum(nums, target);
+    Console.WriteLine($"Example 1 Solution: ({example[0]},{example[1]})");
+}
+
+void Example2()
+{
+    var solution = new Solution();
+    var nums = new[] { 3, 2, 4 };
+    const int target = 6;
+
+    var example = solution.TwoSum(nums, target);
+    Console.WriteLine($"Example 2 Solution: ({example[0]},{example[1]})");
+}
+
+void Example3()
+{
+    var solution = new Solution();
+    var nums = new[] { 3, 3 };
+    const int target = 6;
+    
+    var example = solution.TwoSum(nums, target);
+    Console.WriteLine($"Example 3 Solution: ({example[0]},{example[1]})");
+}
+
+void Example4()
+{
+    var solution = new Solution();
+    var nums = new[] { 3, 5, 5, 11 };
+    const int target = 10;
+    
+    var example = solution.TwoSum(nums, target);
+    Console.WriteLine($"Example 3 Solution: ({example[0]},{example[1]})");
+}
+
---
+++ src/P1/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P1
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = 1; j < nums.Length; j++)
+                {
+                    if (i == j)
+                        continue;
+                    
+                    if (nums[i] + nums[j] == target)
+                        return new[] { i, j };
+                }
+            }
+
+            throw new Exception("No Solution Found");
+        }
+    }
+}
